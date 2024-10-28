a6722624da472ec565eea51ede34f4df25a8f644
Sandra Moya Ortega <san.mo.dra5@gmail.com>
2024-10-28 17:36:26 +0100

    Last Schema

--- models/record.js
+++ models/record.js
@@ -1,24 +1,41 @@
 const mongoose = require("mongoose");
 
 let appointmentsSchema = new mongoose.Schema({
-    date: {
-        type: Date,
-        required: true,
-    }
-})
+  date: {
+    type: Date,
+    required: true
+  },
+  physio: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "physio"
+  },
+  diagnosis: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 500
+  },
+  treatment:{
+    type: String,
+    required: true
+  },
+  observations:{
+    type: String, 
+    maxlength: 500
+  }
+});
 
 let recordsSchema = new mongoose.Schema({
   patient: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'patients'
+    ref: "patients",
   },
   medicalRecord: {
     type: String,
     maxlength: 1000,
   },
-  appointments:{
-  }
+  appointments: [appointmentsSchema]
 });
 
-let Record = mongoose.model('records', recordsSchema);
+let Record = mongoose.model("records", recordsSchema);
 module.exports = Record;
